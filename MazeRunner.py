c35ee5143f91670c24dffc3377c2f048b9f1e87a
Tara <taranidevi12@gmail.com>
2024-05-06 21:55:19 +0800

    merge main int Tara

--- MazeRunner.py
+++ MazeRunner.py
@@ -15,3 +15,20 @@ maze = [
     [1, 0, 0, 0, 0, 0, 0, 1]
     [1, 1, 1, 1, 1, 1, 1, 1]
 ]
+player_pos = [1, 0]
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Maze Runner")
+while True;
+    screen.fill(WHITE)
+    for y in range(len(maze));
+        for x in range(len(maze[y]));
+            if maze[y][x] == 1;
+                pygame.draw.rect(screen, BLUE, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE))
+
+for event in pygame.event.get();
+    if event.type == pygame.QUIT;
+        pygame.quit()
+        sys.exit()
+
+pygame.display.flip()
+
