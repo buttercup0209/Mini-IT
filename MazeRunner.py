decb01bd28ca2907556be29cd1841ab7f782d277
Kashvinna Anne <your@email.com>
2024-05-07 11:18:18 +0800

    merge main into kash

---
+++ MazeRunner.py
@@ -0,0 +1,12 @@
+import pygame
+import sys 
+import time 
+import random 
+
+window_width = 1000
+window_height = 600
+window_title = pygame.display.set_caption("Maze Runner")
+window = pygame.display.set_mode((window_width, window_height))
+
+
+
