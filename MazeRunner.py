d6a5cdd8d598e8d59127ae00d16558dbc3eeb436
Girin <girindrashinie@gmail.com>
2024-05-14 10:07:17 +0800

    line 15-41 & line 47, creating the setting button

--- MazeRunner.py
+++ MazeRunner.py
@@ -12,9 +12,39 @@ window_height = 600
 window_title = pygame.display.set_caption("Maze Runner")
 window = pygame.display.set_mode((window_width, window_height))
 
+#assets
+setting = pygame.image.load("./assets/setting_symbol.png")
+
+#Game variable 
+start_game = False
+
+#class button 
+class Button() :
+    def __init__(self, x, y, image) :
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        
+    def draw(self) :
+        action = False
+        pos = pygame.mouse.get_pos()
+        
+        if self.rect.collidepoint(pos) :
+            if pygame.mouse.get_pressed()[0] == 1 :
+                action = True
+        
+        window.blit(self.image, (self.rect.x, self.rect.y))
+        
+        return action
+    
+#buttons
+setting_button = Button(900, 10, setting)
+
 run = True
 while run : 
     clock.tick(FPS)
+    
+    setting_button.draw() == True
     
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
