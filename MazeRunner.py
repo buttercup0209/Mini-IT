ab01dc597bee1725df03ea947bc71d81860fc4e7
girindrashinie <girindrashinie@gmail.com>
2024-05-20 17:15:43 +0800

    deleting line 6-56 and importing * from tkinter

--- MazeRunner.py
+++ MazeRunner.py
@@ -2,56 +2,64 @@ import pygame
 import time 
 import random 
 import tkinter as tk
+from tkinter import *
 
-pygame.init()
+def login(): 
+    root = tk.Tk()
+    root.title("Maze Runner | Login")
+    root.geometry('350x200')
+    root.columnconfigure(0, weight= 1)
+    root.columnconfigure(1, weight= 1)
+    root.columnconfigure(2, weight= 1)
 
-clock = pygame.time.Clock()
-FPS = 60 
-
-window_width = 1000
-window_height = 600
-window_title = pygame.display.set_caption("Maze Runner")
-window = pygame.display.set_mode((window_width, window_height))
-
-#assets
-setting = pygame.image.load("./assets/setting_symbol.png")
-
-#Game variable 
-start_game = False
-
-#class button 
-class Button() :
-    def __init__(self, x, y, image) :
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
-        
-    def draw(self) :
-        action = False
-        pos = pygame.mouse.get_pos()
-        
-        if self.rect.collidepoint(pos) :
-            if pygame.mouse.get_pressed()[0] == 1 :
-                action = True
-        
-        window.blit(self.image, (self.rect.x, self.rect.y))
-        
-        return action
+    username = Label(root, text = "Username")
+    username.grid(row=0, column=1)
     
-#buttons
-setting_button = Button(900, 10, setting)
+    UserEntry = Entry(root, width=20, borderwidth= 5)
+    UserEntry.grid(row=1, column=1)
+    
+    Password = Label(root, text = "Password")
+    Password.grid(row=2, column=1)
+    
+    PwEntry = Entry(root, width=20, borderwidth= 5)
+    PwEntry.grid(row=3, column=1)
 
-run = True
-while run : 
-    clock.tick(FPS)
-    
-    setting_button.draw() == True
-    
-    for event in pygame.event.get() :
-        if event.type == pygame.QUIT :
-            run = False
-            
-    pygame.display.update()
-    
-pygame.quit() 
+    def clicked() :
+        Login = Button(root, text = "Log In", fg = "black", command=clicked)
+        Login.grid(row= 5, column=1)
 
+    def back() :
+        root.destroy()
+        home()
+
+        back_btn = Button(root, text = "Back", fg = "black", command=back)
+        back_btn.grid(row= 5, column=2)
+
+def home():
+    root = tk.Tk()
+    root.title("Maze Runner")
+    root.geometry('350x200')
+    root.columnconfigure(0, weight= 1)
+    root.columnconfigure(1, weight= 1)
+    root.columnconfigure(2, weight= 1)
+
+    Login_Reg = Label(root, text = "Log In/Register" )
+    Login_Reg.grid(column=1, row=0)
+
+    def clicked() :
+        root.destroy()
+        login()
+        
+    Login = Button(root, text = "Log In", fg = "black", command=clicked)
+    Login.grid(column=0, row=2)
+    
+    def clicked() :
+        root.destroy()
+        age()
+
+    Register = Button(root, text = "Register", fg = "black", command=clicked)
+    Register.grid(column=2, row=2)
+
+    root.mainloop()
+
+home()
