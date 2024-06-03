9533048520b8ce58ee5947adf305468a00656532
Tara <taranidevi12@gmail.com>
2024-06-03 22:59:09 +0800

    added player class and movement

--- MazeRunner.py
+++ MazeRunner.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 
@@ -7,7 +8,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Maze Runner")
 
 white = (255, 255, 255)
-gray = (254, 254, 254)
+gray = (255, 255, 255)
 
 room_width, room_height = 600, 400
 wall_thickness = 10
@@ -23,6 +24,28 @@ walls = [
 ceiling = pygame.Rect(0, 0, room_width, wall_thickness)
 floor = pygame.Rect(0, room_height - wall_thickness, room_width, wall_thickness)
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.image.load(os.path.join('images', 'Ground (78x58).png')).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.center = (room_width // 2, room_height//2)
+
+    def update(self):
+        keys = pygame.key.get_pressed
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= 5
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += 5
+        if keys[pygame.K_UP]:
+            self.rect.y -= 5
+        if keys[pygame.K_DOWN]:
+            self.rect.y += 5
+
+player = Player()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 running = True
 while running:
@@ -30,12 +53,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    all_sprites.update()
     
     screen.fill(white)
     pygame.draw.rect(screen, gray, ceiling)
     pygame.draw.rect(screen, gray, floor)
     for wall in walls:
         pygame.draw.rect(screen, gray, wall)
+    all_sprites.draw(screen)
 
     pygame.display.flip()
 
