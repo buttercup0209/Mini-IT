be69236d305b7870ffab2a2fae1ca317e616b390
Mithraa Liora <mithraaliora@gmail.com>
2024-06-04 11:04:37 +0800

    line 28-49, character and mo0vements

--- MazeRunner.py
+++ MazeRunner.py
@@ -25,44 +25,6 @@ walls = [
 ceiling = pygame.Rect(0, 0, room_width, wall_thickness)
 floor = pygame.Rect(0, room_height - wall_thickness, room_width, wall_thickness)
 
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    
-    screen.fill(white)
-    pygame.draw.rect(screen, gray, ceiling)
-    pygame.draw.rect(screen, gray, floor)
-    for wall in walls:
-        pygame.draw.rect(screen, gray, wall)
-
-    pygame.display.flip()
-
-
-screen_width, screen_height = 800, 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Maze Runner")
-
-white = (255, 255, 255)
-gray = (255, 255, 255)
-
-room_width, room_height = 600, 400
-wall_thickness = 10
-
-walls = [
-    pygame.Rect(0, 0, room_width, wall_thickness), 
-    pygame.Rect(0, 0, wall_thickness, room_height),  
-    pygame.Rect(room_width - wall_thickness, 0, wall_thickness, room_height),  
-    pygame.Rect(0, room_height - wall_thickness, room_width, wall_thickness),  
-]
-
-
-ceiling = pygame.Rect(0, 0, room_width, wall_thickness)
-floor = pygame.Rect(0, room_height - wall_thickness, room_width, wall_thickness)
-
 class Player(pygame.sprite.Sprite):
     def _init_(self):
         super()._init_()
